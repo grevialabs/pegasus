e9a6151ff59268b03131006ec2968b450b3139ee
Rusdi Karsandi <rusdi.karsandi@kawanlamaretail.com>
2018-12-11 17:45:41 +0700

    install django with codename demonix & dexter

---
+++ excel/ifelse.py
@@ -0,0 +1,9 @@
+name = 'demo'
+returx = ''
+
+if name != 'demo':
+    returx = 'iamdemo'
+else :
+    returx = 'notdemo'
+
+print('Hello, ' + returx)
--- excel/read.py
+++ excel/read.py
@@ -1,4 +1,5 @@
 
 from openpyxl import load_workbook
-wb2 = load_workbook('hello world.xlsx')
+# wb2 = load_workbook('hello world.xlsx')
+wb2 = load_workbook('yamete.xlsx')
 print(wb2.sheetnames)
